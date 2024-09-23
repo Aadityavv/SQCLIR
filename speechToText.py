b14483b73bed69b8858a286a28af0b27e45da7ce
Aadityavv <aadityavv9@gmail.com>
2024-09-24 01:27:39 +0530

    new files added

--- speechToText.py
+++ speechToText.py
@@ -1,16 +1,15 @@
 import speech_recognition as sr
 from googletrans import Translator
+from gtts import gTTS
+import os
 
-# Initialize recognizer
 r = sr.Recognizer()
 
-# Use the microphone as source for input
 with sr.Microphone() as source:
     print("Speak something...")
     audio = r.listen(source)
 
     try:
-        # Convert speech to text
         text = r.recognize_google(audio)
         print("You said:", text)
     except sr.UnknownValueError:
@@ -18,16 +17,13 @@ with sr.Microphone() as source:
     except sr.RequestError:
         print("Sorry, the service is not available")
 
-
-
-
-# Initialize translator
 translator = Translator()
 
-# English text to translate
-
-# Translate into Hindi
 translated = translator.translate(text, src='en', dest='hi')
 
-# Print the translated text
 print("Translated text:", translated.text)
+
+language = 'hi'
+speech = gTTS(text=translated.text, lang=language, slow=False)
+speech.save("output.mp3")
+os.system("start output.mp3")
